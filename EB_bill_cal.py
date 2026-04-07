8c9929f80eae1900f05023cea14b289a79ebb68e
Krithika U <krithikaumasankar2007@gmail.com>
2026-04-07 21:51:06 +0530

    Create EB_bill_cal.py

---
+++ EB_bill_cal.py
@@ -0,0 +1,20 @@
+class EB:
+    def __init__(self):
+        print("\n\tEB BILL CALCULATOR...!")
+        self.units=int(input("Enter number of units: "))
+    def display (self):
+        amo=0
+        if self.units <= 100:
+            amo="No amount"
+        elif self.units > 100 and self.units <= 200:
+            amo += (self.units-100)*1.5
+        elif self.units > 200 and self.units <= 300:
+            amo += (self.units-200)*2.5 + (100*1.5)
+        elif self.units > 300 and self.units <= 400:
+            amo += (self.units-300)*4 + (100*1.5) + (100*2.5)
+        elif self.units > 400:
+            amo += (self.units-400)*5 + (100*4) + (100*2.5) + (100*1.5)
+        print("\nUnits used:",self.units,"\nElectricity Bill Amount:",amo)
+
+e=EB()
+e.display()
